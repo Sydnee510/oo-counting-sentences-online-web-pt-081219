bec4278ecbda6bb6fa81863d4f57b23b34e95ed0
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-09-01 04:03:30 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,7 +16,7 @@ class String
 
   def count_sentences
     # self.split.count
-     self.split(/[.!?]/).reject {|x| x.empty?}.size
+     self.split(/[.!?]/).reject {|string| string.empty?}.size
   end
   
 end
